1581b969ca6821115a00a32d6eb8cc440bbae400
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2013-02-01 00:14:48 -0800

    Adding ported V2v forwarding strategy

---
+++ extensions/ndn-fw-v2v.cc
@@ -0,0 +1,149 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2011 University of California, Los Angeles
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Alexander Afanasyev <alexander.afanasyev@ucla.edu>
+ */
+
+#include "ndn-fw-v2v.h"
+#include "ndn-v2v-net-device-face.h"
+
+#include <ns3/log.h>
+#include <ns3/ptr.h>
+#include <ns3/assert.h>
+
+#include <ns3/ndn-l3-protocol.h>
+#include <ns3/ndn-pit-entry.h>
+#include <ns3/ndn-fib-entry.h>
+#include <ns3/ndn-interest.h>
+#include <ns3/ndn-content-object.h>
+#include <ns3/ndn-content-store.h>
+
+#include <boost/foreach.hpp>
+
+NS_LOG_COMPONENT_DEFINE ("ndn.fw.V2v");
+
+namespace ns3 {
+namespace ndn {
+namespace fw {
+
+NS_OBJECT_ENSURE_REGISTERED (V2v);
+
+TypeId
+V2v::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::ndn::fw::V2v")
+    .SetParent<ForwardingStrategy> ()
+    .SetGroupName ("Ndn")
+    .AddConstructor<V2v> ()
+
+  ;
+  return tid;
+}
+
+V2v::V2v()
+{
+}
+
+V2v::~V2v ()
+{
+}
+
+bool
+V2v::DoPropagateInterest (Ptr<Face> inFace,
+                          Ptr<const InterestHeader> header,
+                          Ptr<const Packet> origPacket,
+                          Ptr<pit::Entry> pitEntry)
+{
+  NS_LOG_FUNCTION (this);
+
+  int propagatedCount = 0;
+
+  BOOST_FOREACH (const fib::FaceMetric &metricFace, pitEntry->GetFibEntry ()->m_faces.get<fib::i_metric> ())
+    {
+      NS_LOG_DEBUG ("Trying " << boost::cref(metricFace));
+      if (metricFace.m_status == fib::FaceMetric::NDN_FIB_RED) // all non-read faces are in the front of the list
+        break;
+
+      if (!TrySendOutInterest (inFace, metricFace.m_face, header, origPacket, pitEntry))
+        {
+          continue;
+        }
+
+      propagatedCount++;
+    }
+
+  NS_LOG_INFO ("Propagated to " << propagatedCount << " faces");
+  return propagatedCount > 0;
+}
+
+
+void
+V2v::DidReceiveUnsolicitedData (Ptr<Face> inFace,
+                                Ptr<const ContentObjectHeader> header,
+                                Ptr<const Packet> payload,
+                                Ptr<const Packet> origPacket)
+{
+  // Opportunistically add or update entry in the content store
+  bool newEntry = m_contentStore->Add (header, payload);
+  if (!newEntry)
+    {
+      NS_LOG_DEBUG ("ContentObject is already in cache. Don't start low-priority processing");
+      // stop processing if data was already in cache
+      return;
+    }
+
+  Ptr<L3Protocol> l3 = this->GetObject<L3Protocol> ();
+  NS_ASSERT (l3 != 0);
+
+  // Push interest using low-priority send method
+  for (uint32_t faceId = 0; faceId < l3->GetNFaces (); faceId++)
+    {
+      TrySendLowPriority (l3->GetFace (faceId), origPacket);
+    }
+}
+
+void
+V2v::DidExhaustForwardingOptions (Ptr<Face> inFace,
+                                  Ptr<const InterestHeader> header,
+                                  Ptr<const Packet> origPacket,
+                                  Ptr<pit::Entry> pitEntry)
+{
+  // not sure if outgoing faces should be removed or not... could cause the trouble
+
+  ForwardingStrategy::DidExhaustForwardingOptions (inFace, header, origPacket, pitEntry);
+
+  // Try to push new packet further with lowest priority possible on all L3 faces
+  BOOST_FOREACH (const fib::FaceMetric &face, pitEntry->GetFibEntry ()->m_faces)
+    {
+      TrySendLowPriority (face.m_face, origPacket);
+    }
+}
+
+void
+V2v::TrySendLowPriority (Ptr<Face> face, Ptr<const Packet> packet)
+{
+  Ptr<V2vNetDeviceFace> v2vFace = DynamicCast<V2vNetDeviceFace> (face);
+  if (v2vFace)
+    {
+      v2vFace->SendLowPriority (packet->Copy ());
+    }
+}
+
+
+} // namespace fw
+} // namespace ndn
+} // namespace ns3
---
+++ extensions/ndn-fw-v2v.h
@@ -0,0 +1,74 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2011 University of California, Los Angeles
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Alexander Afanasyev <alexander.afanasyev@ucla.edu>
+ */
+
+#ifndef NDN_FW_V2V_H
+#define NDN_FW_V2V_H
+
+#include "ns3/ndn-forwarding-strategy.h"
+
+namespace ns3 {
+namespace ndn {
+namespace fw {
+
+/**
+ * \ingroup Ndn
+ * \brief Implementation of NDN forwarding strategy for V2V communications
+ */
+class V2v : public ForwardingStrategy
+{
+public:
+  static TypeId GetTypeId ();
+
+  /**
+   * \brief Default constructor. Creates an empty stack without forwarding strategy set
+   */
+  V2v();
+  virtual ~V2v ();
+
+protected:
+  // from ForwardingStrategy
+  bool
+  DoPropagateInterest (Ptr<Face> inFace,
+                       Ptr<const InterestHeader> header,
+                       Ptr<const Packet> origPacket,
+                       Ptr<pit::Entry> pitEntry);
+
+
+  void
+  DidExhaustForwardingOptions (Ptr<Face> inFace,
+                               Ptr<const InterestHeader> header,
+                               Ptr<const Packet> origPacket,
+                               Ptr<pit::Entry> pitEntry);
+
+  void
+  DidReceiveUnsolicitedData (Ptr<Face> inFace,
+                             Ptr<const ContentObjectHeader> header,
+                             Ptr<const Packet> payload,
+                             Ptr<const Packet> origPacket);
+private:
+  void
+  TrySendLowPriority (Ptr<Face> face, Ptr<const Packet> packet);
+};
+
+} // namespace fw
+} // namespace ndn
+} // namespace ns3
+
+#endif /* CCNX_L3_PROTOCOL_H */
